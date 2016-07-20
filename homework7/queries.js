aee47880b38d782a19702f8f28be25c9addf6d53
Daria Novikova <dashunay@gmail.com>
2016-07-20 22:39:30 +0300

    h7: Query 2

--- homework7/queries.js
+++ homework7/queries.js
@@ -1,2 +1,4 @@
 //task 1: 
 db.academy.find({ scores: { $elemMatch: { $and: [{ score: { $gt: 87 } }, { score: { $lt: 93 } }] } } }).pretty();
+//task 2: 
+db.academy.aggregate({ $unwind: "$scores" }, { $match: { "scores.type": "exam", "scores.score": { $gt: 90 } } }, { $project: { name: "$name", "score": "$scores.score" } });
