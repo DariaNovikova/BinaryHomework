2c937482428fbdf866d4b232e914be74a9389e9e
Daria Novikova <dashunay@gmail.com>
2016-07-24 12:03:49 +0300

    h8: Added MongoDB persistence

--- homework8/server.js
+++ homework8/server.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoClient = require('mongodb').MongoClient;
 var app = express();
-
-var messages = [{
-    name:'jeka', content:'jjjjjbhvhgcvmhgcgfm', date: new Date()
-}];
+var messages;
 
 app.use(express.static('site'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     console.log({
@@ -21,14 +19,34 @@ app.use(function (req, res, next) {
 });
 
 app.get('/api/message', (req, res) => {
-    res.json(messages);
+    messages.find({}).toArray((err, result) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
 });
 
 app.post('/api/message', (req, res) => {
-    messages.push(req.body);
-    res.end();
+    messages.insert(req.body, (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
 })
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+mongoClient.connect('mongodb://localhost:27017/chat', (err, database) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        messages = database.collection('messages');
+        console.log('Connected to MongoDB');
+        app.listen(3000, () => console.log('Example app listening on port 3000!'));
+    }
 });
